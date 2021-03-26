3a68fa144f24b4123d344a95e225fd323c9ea298
tamburrisam <tamburrisam@gmail.com>
2021-03-26 16:51:20 -0400

    added newProj class

--- src/DOMProjectsSidebar.js
+++ src/DOMProjectsSidebar.js
@@ -1,13 +1,16 @@
 import {projects} from './index.js'
+import { Project } from "./createProject.js";
+
+
 
 const projectAdd = () => {
     const input = document.createElement('input');
     const br = document.createElement('br');
     const submit = document.createElement('button');
     const projectAdd = document.querySelector('#projectadd');
-const projectSub = document.querySelector('#projectsubmit');
-const projectInput = document.querySelector('.bookInput');
-const h2 = document.querySelector('.main');
+    const projectSub = document.querySelector('#projectsubmit');
+    const projectInput = document.querySelector('.bookInput');
+    const h2 = document.querySelector('.main');
 
 
     h2.appendChild(br);
@@ -21,10 +24,19 @@ const h2 = document.querySelector('.main');
     
      submit.addEventListener('click', function(){
         const div = document.createElement('div');
+        div.setAttribute('class', 'projecttitles')
         div.innerHTML = input.value;
         projects.push(input.value)
 
         localStorage.setItem('projects', JSON.stringify(projects))
+        
+        /* testing project object */
+        const newProj = new Project(`${input.value}`, `${projects.length}`)
+
+        console.log(newProj)
+
+
+
         console.log(projects)
 
         input.style.display = 'none';

3b88940b2864d1d6611179660e4b3d9c8bed06ab
tamburrisam <tamburrisam@gmail.com>
2021-03-26 15:52:20 -0400

    cleaned up entry

--- src/index.js
+++ src/index.js
@@ -1,67 +1,22 @@
-import {Project} from './createProject.js'
+import { Project } from "./createProject.js";
+import { projectAdd as projectDom1 } from "./DOMProjectsSidebar.js";
+import { ifPageLoaded, lsItems } from "./localStorageItems";
 
+export let projects = [];
 
-let projects = [];
+let user = new Project("Crap");
+console.log(user.sayName());
 
+const projectAdd = document.querySelector("#projectadd");
 
-let user = new Project('Crap')
-console.log(user.sayName())
-
-const projectAdd = document.querySelector('#projectadd');
-const projectSub = document.querySelector('#projectsubmit');
-const projectInput = document.querySelector('.bookInput');
-const h2 = document.querySelector('.main');
-let lsItems = JSON.parse(localStorage.getItem("projects"));
-
-if(lsItems){
-    projects = lsItems;
+if (lsItems) {
+  projects = lsItems;
 }
 
-const ifPageLoaded = () => {
-    const main = document.querySelector('.main');
-    if (localStorage.getItem("projects") !== null) {
-  //projects for each attach to main 
-      projects.forEach((element) => {
-          const div = document.createElement('div');
-          div.innerHTML = element;
-          h2.appendChild(div)
-          console.log(element)
-      })
-    }
-  
-  };
-document.addEventListener("DOMContentLoaded", ifPageLoaded()); 
+document.addEventListener("DOMContentLoaded", ifPageLoaded());
 
+projectAdd.addEventListener("click", function () {
+  projectDom1();
+});
 
-projectAdd.addEventListener('click', function(){
-    const input = document.createElement('input');
-    const br = document.createElement('br');
-    const submit = document.createElement('button');
-
-    h2.appendChild(br);
-    h2.appendChild(input);
-    input.setAttribute("type", "text");
-    input.setAttribute('class', 'bookInput')
-    h2.appendChild(submit);
-    submit.innerHTML = 'Submit'
-    submit.setAttribute('id', 'projectsubmit');
-
-    
-     submit.addEventListener('click', function(){
-        const div = document.createElement('div');
-        div.innerHTML = input.value;
-        projects.push(input.value)
-
-        localStorage.setItem('projects', JSON.stringify(projects))
-        console.log(projects)
-
-        input.style.display = 'none';
-        submit.style.display = 'none';
-        h2.appendChild(div);
-
-     
-    })
-
-})
-
-console.log(projects)
+console.log(projects);

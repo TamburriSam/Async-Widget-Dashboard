509ffea14bd00f20f52f85f451253260954b1c8f
tamburrisam <tamburrisam@gmail.com>
2021-03-29 11:32:24 -0400

    added populate notes function

--- src/DOMProjectsSidebar.js
+++ src/DOMProjectsSidebar.js
@@ -1,15 +1,13 @@
-import {projects} from './index.js'
+import {projects, titles, projectName} from './index.js'
 import { Project } from "./createProject.js";
 
 
+const todoList = document.querySelector('#todolist');
 
 const projectAdd = () => {
     const input = document.createElement('input');
     const br = document.createElement('br');
     const submit = document.createElement('button');
-    const projectAdd = document.querySelector('#projectadd');
-    const projectSub = document.querySelector('#projectsubmit');
-    const projectInput = document.querySelector('.bookInput');
     const h2 = document.querySelector('.main');
 
 
@@ -21,28 +19,89 @@ const projectAdd = () => {
     submit.innerHTML = 'Submit'
     submit.setAttribute('id', 'projectsubmit');
 
+    let projectFolder = [];
+
     
      submit.addEventListener('click', function(){
         const div = document.createElement('div');
-        div.setAttribute('class', 'projecttitles')
-        div.innerHTML = input.value;
-        projects.push(input.value)
 
-        localStorage.setItem('projects', JSON.stringify(projects))
+        div.setAttribute('class', 'projecttitles')
+
+        div.innerHTML = input.value;
+        /* projects.push(input.value) */
+
+        /* localStorage.setItem('projects', JSON.stringify(projects)) */
         
         /* testing project object */
-        const newProj = new Project(`${input.value}`, `${projects.length}`)
+         const newProj = new Project(`${input.value}`, `${projects.length}`)
 
-        console.log(newProj)
+      
+         projects.push(newProj)
+
+         localStorage.setItem('projects', JSON.stringify(projects));
+
+         projectFolder.push(newProj.name)
+
+        console.log(newProj.id)
+        console.log(newProj) 
+
 
 
 
         console.log(projects)
+        console.log(newProj.name)
 
         input.style.display = 'none';
         submit.style.display = 'none';
         h2.appendChild(div);
+
+        div.addEventListener('click', function(){
+            var current = document.getElementsByClassName("active");
+          if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+          }
+      
+          projectName.innerHTML = this.innerHTML;
+          this.className += " active";
+        })
 })
 }
 
-export {projectAdd}
+const activeProject = () => {
+    titles.forEach((item) => {
+        item.addEventListener("click", function () {
+          console.log(item);
+      
+          var current = document.getElementsByClassName("active");
+      
+          if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+          }
+     
+          projectName.innerHTML = this.innerHTML;
+          this.className += " active";
+
+          //populate todosection w list array
+
+
+          for(let i = 0; i < projects.length; i++){
+              projects[i].list.forEach((element) => {
+                  console.log(element);
+                  const div = document.createElement('div');
+
+                  div.innerHTML = element;
+                  todoList.appendChild(div)
+              })
+          }
+
+
+        });
+      });
+}
+
+
+
+
+
+
+export {projectAdd, activeProject}

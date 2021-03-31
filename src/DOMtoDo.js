ba69c15a68eb12373880957811ef577367f885ab
tamburrisam <tamburrisam@gmail.com>
2021-03-31 16:44:21 -0400

    tweaked newtask function

--- src/DOMtoDo.js
+++ src/DOMtoDo.js
@@ -15,7 +15,7 @@ function newTask(){
     //const input = new Task(task, date, priority) maybe make into object
    
     input.setAttribute('class', 'list-styling')
-    input.innerHTML = `${taskField} <span class="dueDate"> Due: ${dateField} </span>
+    input.innerHTML = `<div class ="taskName">${taskField}</div> <span class="dueDate"> Due: ${dateField} </span>
     <div id="icons">
     <button class = "ex"><i class="fas fa-trash"></i></button> 
      <button class ="edit"><i class="fas fa-edit"></i></button>
@@ -28,12 +28,10 @@ function newTask(){
             /* item.list.push(`${taskField} ${dateField}`) ; */
             let newO = {task: `${taskField}`, date: `${dateField}`, priority: `${priorityField}`};
             item.list.push(newO)
-            console.log(item)
             //right here
             item.date.push(dateField);
             
             localStorage.setItem('projects', JSON.stringify(projects));
-            console.log(projects)
 
            
 
@@ -42,6 +40,8 @@ function newTask(){
 
 
 }
+
+
 
 
 

ca8c56dbdb97801f2394f7df5325f4156004f22d
tamburrisam <tamburrisam@gmail.com>
2021-04-05 14:09:12 -0400

    changing obj structure

--- src/createProject.js
+++ src/createProject.js
@@ -1,12 +1,12 @@
- class Project{
-constructor(name, id, list = [], date = []){
-        this.name = name;
-        this.id = id;
-        this.list = list;
-        this.date = date;
-    }
-
-
+class Project {
+  constructor(name, id, list = [], date = []) {
+    this.name = name;
+    this.id = id;
+    this.list = list;
+    this.date = date;
+  }
 }
 
-export {Project} 
+//look into making a function that displays the items wanted out of a task w arguments. the projects i list for each part where you stuff the div EH IDK MAYBE NOT
+
+export { Project };

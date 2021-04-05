aaabb24a92e30d58847219452e4c3b44d11112d7
tamburrisam <tamburrisam@gmail.com>
2021-04-05 14:08:54 -0400

    added edit on modal. added radio btns

--- src/modal.js
+++ src/modal.js
@@ -1,133 +1,86 @@
-import { da } from 'date-fns/locale';
-import { divide } from 'lodash';
-import {deleteTask2} from './DOMProjectsSidebar.js'
-import {newTask} from './DOMtoDo.js';
+import { deleteTask2 } from "./DOMProjectsSidebar.js";
+import { newTask } from "./DOMtoDo.js";
 
 const openModal = () => {
-//list styling
-let trigger = document.querySelector('.list-styling');
+  //list styling
+  let trigger = document.querySelector(".edit");
 
-//modal box appear
-let modal = document.getElementById("myModal");
+  //modal box appear
+  let modal = document.getElementById("myModal");
 
-//modal box close
-let span = document.getElementsByClassName('close')[0];
+  //modal box close
+  let span = document.getElementsByClassName("close")[0];
 
+  //list styling
+  trigger.onclick = function () {
+    let target = this.parentElement.parentElement.childNodes[0];
 
-//list styling
-trigger.onclick = function(){
+    let complete = this.parentElement.parentElement;
 
+    modal.style.display = "block";
 
-    let target = this.childNodes[0];
+    let taskModal, dateModal, priorityModal, notesModal;
 
-    let complete = this;
+    let modalContent = document.querySelector(".modal-content");
 
+    taskModal = document.createElement("input");
 
+    taskModal.setAttribute("class", "task-modal");
 
+    taskModal.value = this.parentElement.parentElement.childNodes[0].textContent;
 
-    modal.style.display='block';
-    
-    let taskModal,
-    dateModal,
-    priorityModal,
-    notesModal;
+    let label = document.createElement("div");
+    label.innerHTML = '<div class="task-label">Task</div>';
 
-    let modalContent = document.querySelector('.modal-content');
+    let date = document.createElement("div");
+    date.innerHTML = '<div class="date-label">Date</div>';
 
-    /* modalContent.innerHTML = `<label for="task-input>
-    <input type ` */
+    let prio = document.createElement("div");
+    prio.innerHTML = '<div class="prio-label">Prio</div>';
 
+    dateModal = document.createElement("input");
+    dateModal.value = this.parentElement.parentElement.childNodes[2].textContent;
+    dateModal.setAttribute("class", "date-modal");
 
-taskModal = document.createElement('input');
+    //make radio btn instead
+    priorityModal = document.createElement("div");
 
-taskModal.setAttribute('class', 'task-modal');
+    /*   priorityModal.value = this.parentElement.parentElement.childNodes[4].textContent; */
+    priorityModal.setAttribute("class", "prio-modal");
+    priorityModal.innerHTML = `  <select name="cars" id="prio">
+    <option value="high-priority">High</option>
+    <option value="medium-priority">Medium</option>
+    <option value="low-priority">Low</option>
+  </select>`;
 
+    notesModal = document.createElement("input");
+    notesModal.setAttribute("class", "notes-modal");
 
-taskModal.value = this.childNodes[0].textContent;
+    modalContent.innerHTML = "";
 
+    modalContent.appendChild(label);
+    modalContent.appendChild(date);
+    modalContent.appendChild(prio);
+    modalContent.appendChild(taskModal);
+    modalContent.appendChild(dateModal);
+    modalContent.appendChild(priorityModal);
 
+    //modalContent.appendChild(notesModal);
 
-let label = document.createElement('div');
-label.innerHTML = '<div class="task-label">Task</div>'
+    let modalSubmit = document.createElement("btn");
+    modalSubmit.setAttribute("class", "modal-submit");
+    modalSubmit.innerHTML = "Submit";
 
-let date = document.createElement('div');
-date.innerHTML = '<div class="date-label">Date</div>'
+    modalContent.appendChild(modalSubmit);
 
-let prio = document.createElement('div');
-prio.innerHTML = '<div class="prio-label">Prio</div>'
+    modalSubmit.addEventListener("click", function () {
+      deleteTask2(target.textContent, complete);
+      console.log(complete);
+      let secondPrio = document.getElementById("prio");
+      newTask(taskModal.value, dateModal.value, secondPrio.value);
+      modal.style.display = "none";
+    });
+  };
+};
 
-
-
-
-
-dateModal = document.createElement('input')
-dateModal.value = this.childNodes[2].textContent;
-dateModal.setAttribute('class', 'date-modal');
-
-
-
-
-//make radio btn instead 
-priorityModal = document.createElement('input')
-priorityModal.value = this.childNodes[4].textContent
-priorityModal.setAttribute('class', 'prio-modal');
-
-
-notesModal = document.createElement('input');
-notesModal.setAttribute('class', 'notes-modal');
-
-
-modalContent.innerHTML = '';
-
-modalContent.appendChild(label);
-modalContent.appendChild(date);
-modalContent.appendChild(prio);
-modalContent.appendChild(taskModal)
-modalContent.appendChild(dateModal);
-modalContent.appendChild(priorityModal);
-
-
-
-let modalSubmit = document.createElement('btn');
-modalSubmit.setAttribute('class', 'modal-submit')
-modalSubmit.innerHTML = 'Submit';
-
-modalContent.appendChild(modalSubmit)
-
-modalSubmit.addEventListener('click', function(){
-  
-
-    
-
-    deleteTask2(target.textContent, complete)
-
-    newTask(taskModal.value, dateModal.value, priorityModal.value);
-
-    modal.style.display = 'none';
-    
- 
-
-    
-})
-
-
-
-
-
-}
-
-//modal box close
-span.onclick = function(){
-    modal.style.display="none";
-}
-
-//if clicked outside modal, close window
-window.onclick = function(e){
-    if(e.target == modal){
-        modal.style.display="none";
-    }
-}
-
-}
-
-export {openModal}
+export { openModal };

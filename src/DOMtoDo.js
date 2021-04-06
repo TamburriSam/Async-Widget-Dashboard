0f84d01aa18545834148f4c4957ea11eb847708e
tamburrisam <tamburrisam@gmail.com>
2021-04-06 13:44:26 -0400

    changed active and delete functions to match project class

--- src/DOMtoDo.js
+++ src/DOMtoDo.js
@@ -1,21 +1,18 @@
-import { projectName, projects, titles } from "./index";
-import { deleteTask } from "./DOMProjectsSidebar";
+import { projectName, projects } from "./index";
+import { Project } from "./DOMProjectsSidebar";
+import { format, formatDistance, formatRelative, subDays } from "date-fns";
 
 export const addList = document.querySelector("#todoadd");
 
 function newTask(task, date, priority) {
-
-
   const input = document.createElement("div");
   const todoList = document.querySelector(".ls-todos");
 
-
   todoList.prepend(input);
-
 
   input.setAttribute("class", "list-styling");
   //clear input?
-  input.innerHTML = '';
+  input.innerHTML = "";
   input.innerHTML = `<div class ="taskName">${task}</div> <span class="dueDate"> ${date} </span>
   <span class="priorityLevel">${priority}
   </span>
@@ -24,26 +21,25 @@ function newTask(task, date, priority) {
      <button class ="edit"><i class="fas fa-edit"></i></button>
      </div>`;
 
- 
   projects.forEach((item) => {
     if (item.name.includes(projectName.textContent)) {
-
       let newO = {
         task: `${task}`,
-        date: `${date}`,
+        date: `${format(new Date(date), "MMM do yyyy")}`,
         priority: `${priority}`,
       };
       item.list.push(newO);
-      item.date.push(date);
-      //right here
+      item.date.push(format(new Date(date), "MMM do yyyy"));
 
+      console.log(format(new Date(date), "MMM do yyyy"));
+      //right here
 
       localStorage.setItem("projects", JSON.stringify(projects));
 
       let x = document.querySelector(".fas");
       x.addEventListener("click", function () {
         //delete item
-        deleteTask(this);
+        Project.deleteTask(this);
       });
     }
   });

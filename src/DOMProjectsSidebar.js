5d8425a38a687e5f732b7a8ae2b562ccdb341162
tamburrisam <tamburrisam@gmail.com>
2021-04-06 13:45:18 -0400

    changed to static methods. added date FN-S manipulation

--- src/DOMProjectsSidebar.js
+++ src/DOMProjectsSidebar.js
@@ -22,117 +22,114 @@ export class Project {
     projectName.innerHTML = activated.innerHTML;
     activated.className += " active";
   }
-}
 
-/* const makeActive = (activated) => {
-  var current = document.getElementsByClassName("active");
+  static findTask(tasked) {
+    for (let i = 0; i < projects.length; i++) {
+      if (projects[i].name.includes(tasked.textContent)) {
+        todoList.innerHTML = "";
 
-  if (current.length > 0) {
-    current[0].className = current[0].className.replace(" active", "");
-  }
+        projects[i].list.forEach((element) => {
+          //clear whatever is in this pane
+          const div = document.createElement("div");
 
-  projectName.innerHTML = activated.innerHTML;
-  activated.className += " active";
-}; */
+          div.innerHTML = `<div class ="taskName">${element.task}</div> 
+          <span class ="dueDate">${element.date}</span>
+          <span class="priorityLevel">${element.priority} </span>   
+          <div id="icons">
+          <button class = "ex"><i class="fas fa-trash"></i></button> 
+           <button class ="edit"><i class="fas fa-edit"></i></button>
+           </div>`;
+          div.setAttribute("class", "list-styling");
 
-const findTask = (tasked) => {
-  for (let i = 0; i < projects.length; i++) {
-    if (projects[i].name.includes(tasked.textContent)) {
-      todoList.innerHTML = "";
+          todoList.prepend(div);
 
-      projects[i].list.forEach((element) => {
-        //clear whatever is in this pane
-        const div = document.createElement("div");
+          let x = document.querySelector(".fa-trash");
 
-        div.innerHTML = `<div class ="taskName">${element.task}</div> 
-        <span class ="dueDate">${element.date}</span>
-        <span class="priorityLevel">${element.priority} </span>   
-        <div id="icons">
-        <button class = "ex"><i class="fas fa-trash"></i></button> 
-         <button class ="edit"><i class="fas fa-edit"></i></button>
-         </div>`;
-        div.setAttribute("class", "list-styling");
-
-        todoList.prepend(div);
-
-        let x = document.querySelector(".fa-trash");
-
-        x.addEventListener("click", function () {
-          //delete item
-          deleteTask(this);
-        });
-
-        openModal();
-
-        let edit = document
-          .querySelector(".fa-edit")
-          .addEventListener("click", function () {
-            console.log("e");
+          x.addEventListener("click", function () {
+            //delete item
+            Project.deleteTask(this);
           });
-      });
+
+          openModal();
+        });
+      }
     }
   }
-};
 
-//add project folder
-const projectAdd = () => {
-  const input = document.createElement("input");
-  const br = document.createElement("br");
-  const submit = document.createElement("button");
-  const h2 = document.querySelector(".main");
+  static projectAdd() {
+    const input = document.createElement("input");
+    const br = document.createElement("br");
+    const submit = document.createElement("button");
+    const h2 = document.querySelector(".main");
 
-  h2.appendChild(br);
-  h2.appendChild(input);
-  input.setAttribute("type", "text");
-  input.setAttribute("class", "bookInput");
-  h2.appendChild(submit);
-  submit.innerHTML = "Submit";
-  submit.setAttribute("id", "projectsubmit");
+    h2.appendChild(br);
+    h2.appendChild(input);
+    input.setAttribute("type", "text");
+    input.setAttribute("class", "bookInput");
+    h2.appendChild(submit);
+    submit.innerHTML = "Submit";
+    submit.setAttribute("id", "projectsubmit");
 
-  submit.addEventListener("click", function () {
-    const div = document.createElement("div");
+    submit.addEventListener("click", function () {
+      const div = document.createElement("div");
 
-    div.setAttribute("class", "projecttitles");
+      div.setAttribute("class", "projecttitles");
 
-    div.innerHTML = input.value;
+      div.innerHTML = input.value;
 
-    /* testing project object */
-    const newProj = new Project(`${input.value}`, `${projects.length}`);
+      /* testing project object */
+      const newProj = new Project(`${input.value}`, `${projects.length}`);
 
-    projects.push(newProj);
+      projects.push(newProj);
 
-    localStorage.setItem("projects", JSON.stringify(projects));
+      localStorage.setItem("projects", JSON.stringify(projects));
 
-    input.style.display = "none";
-    submit.style.display = "none";
-    h2.appendChild(div);
+      input.style.display = "none";
+      submit.style.display = "none";
+      h2.appendChild(div);
 
-    div.addEventListener("click", function () {
-      Project.makeActive(this);
+      div.addEventListener("click", function () {
+        Project.makeActive(this);
 
-      /// find out how to edit and populate upon new folder creation. ri ght now app only works as intended on reload not when folder is created. cant edit folder properly upon first creation.
+        todoList.innerHTML = "";
 
-      console.log("here now");
-
-      todoList.innerHTML = "";
-
-      findTask(this);
+        Project.findTask(this);
+      });
     });
-  });
-};
-
-//active project + populate task items
-const activeProject = () => {
-  titles.forEach((item) => {
-    item.addEventListener("click", function () {
-      Project.makeActive(this);
-      findTask(this);
-      console.log(this.textContent);
+  }
+  static activeProject() {
+    titles.forEach((item) => {
+      item.addEventListener("click", function () {
+        Project.makeActive(this);
+        Project.findTask(this);
+        console.log(this.textContent);
+      });
     });
-  });
-};
+  }
 
-const deleteTask = (task) => {
+  static deleteTask(task) {
+    for (let i = 0; i < projects.length; i++) {
+      if (projects[i].name.includes(projectName.textContent)) {
+        let targ =
+          task.parentElement.parentElement.parentElement.firstChild.textContent;
+
+        var removedIndex = projects[i].list
+          .map(function (x) {
+            return x.task;
+          })
+          .indexOf(targ);
+
+        projects[i].list.splice(removedIndex, 1);
+
+        localStorage.setItem("projects", JSON.stringify(projects));
+
+        task.parentElement.parentElement.parentElement.style.display = "none";
+      }
+    }
+  }
+}
+
+/* const deleteTask = (task) => {
   for (let i = 0; i < projects.length; i++) {
     if (projects[i].name.includes(projectName.textContent)) {
       let targ =
@@ -151,7 +148,7 @@ const deleteTask = (task) => {
       task.parentElement.parentElement.parentElement.style.display = "none";
     }
   }
-};
+}; */
 
 const deleteTask2 = (task, other) => {
   for (let i = 0; i < projects.length; i++) {
@@ -172,11 +169,4 @@ const deleteTask2 = (task, other) => {
   }
 };
 
-export {
-  projectAdd,
-  activeProject,
-  //makeActive,
-  deleteTask,
-  findTask,
-  deleteTask2,
-};
+export { deleteTask2 };

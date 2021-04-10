5badd3251bd7875a72ac58bf9c0def7d953f07d4
tamburrisam <tamburrisam@gmail.com>
2021-04-10 14:46:03 -0400

    future comment;

--- src/localStorageItems.js
+++ src/localStorageItems.js
@@ -1,27 +1,24 @@
- import {projects} from './index.js'
-
-
+import { projects } from "./index.js";
 
 export let lsItems = JSON.parse(localStorage.getItem("projects"));
 
 const ifPageLoaded = () => {
-    const h2 = document.querySelector('.main');
-    if (localStorage.getItem("projects") !== null) {
-      //add mock main page here
-      
+  const h2 = document.querySelector(".main");
+  if (localStorage.getItem("projects") !== null) {
+    //add mock main page here
 
+    //projects for each attach to main
+    projects.forEach((element) => {
+      /* probably put this in a domcontroller */
+      const div = document.createElement("div");
+      div.setAttribute("class", "projecttitles");
+      div.innerHTML = element.name;
+      h2.appendChild(div);
+      console.log(element);
+    });
+  }
+};
 
-  //projects for each attach to main 
-      projects.forEach((element) => {
-          /* probably put this in a domcontroller */
-          const div = document.createElement('div');
-          div.setAttribute('class', 'projecttitles');
-          div.innerHTML = element.name;
-          h2.appendChild(div)
-          console.log(element)
-      })
-    }
-  
-  };
+export { ifPageLoaded };
 
-  export {ifPageLoaded} 
+//put here

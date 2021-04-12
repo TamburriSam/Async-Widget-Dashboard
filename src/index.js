ffc90ce14b06a375c931be6442181b4d114002ea
tamburrisam <tamburrisam@gmail.com>
2021-04-11 23:29:57 -0400

    weatherBox api event listener

--- src/index.js
+++ src/index.js
@@ -53,4 +53,67 @@ let nameField = document.querySelector("#namebox");
 
 nameField.innerHTML = today;
 
-console.log("d33");
+console.log("5");
+
+//waiting to verify api key
+const weatherBox = document.querySelector("#weatherbox");
+weatherBox.addEventListener("click", function () {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={641629d9803f70eee81a8b3336c55851}`,
+    { mode: "cors" }
+  );
+});
+
+/* async function getWeather() {
+  const response = await fetch(
+    `https://fcc-weather-api.glitch.me/api/current?lat=20&lon=60`
+  );
+  const weatherData = await response.json();
+
+  console.log(weatherData);
+}
+
+getWeather(); */
+
+let num = 0;
+
+let p = new Promise(function (resolve, reject) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position.coords.latitude);
+  });
+  if (num === 0) {
+    resolve("Success");
+  } else {
+    reject("failed");
+  }
+}).then((result) => console.log(result));
+
+let d = new Promise((resolve, reject) => {
+  if (p) {
+    resolve("Success again");
+  } else {
+    reject("failed");
+  }
+});
+
+console.log(d);
+
+/* const promise = new Promise((resolve, reject) => {
+  navigator.geolocation.getCurrentPosition(resolve, reject);
+});
+
+promise
+  .then((position) => {
+    return position.coords.longitude;
+  })
+  .catch((error) => console.log(error));
+
+console.log(promise);
+
+ */
+
+/* let latitude = navigator.geolocation.getCurrentPosition((position) => {
+  console.log(position.coords.longitude);
+});
+
+console.log(latitude); */
